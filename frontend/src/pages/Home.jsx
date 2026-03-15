7eb9f7eeb87d5420a0796946fe608167bad2fecb
muluhuneg <muluhunegnaw0@gmail.com>
2026-03-15 13:12:38 +0300

    Update project files

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import './index.css';
 import API from '../services/api';
 import { 
     FaStar, 
